f47597c2721d4e8e1cb6a99f51da8d355b4b490d
Enrique Prado <enrique.prado7@gmail.com>
2025-03-12 00:20:12 -0700

    Add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import logo from './logo.svg'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [clickCount, setClickCount] = useState(0)
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
+        This is a Header
       </header>
+      <div>
+        <button type="button" onClick={() => setClickCount(prevCount => prevCount + 1)}>Click me</button>
+        <p>You clicked {clickCount} times</p>
+      </div>
     </div>
   )
 }
